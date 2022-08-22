d112641fa5ee8480773724a2429cb6232c3aa42f
Prabesh Aryal <73214060+prabesharyal@users.noreply.github.com>
2022-08-22 15:48:11 +0545

    ig-scraper-bot_v1
    A simple bot to scrape instagram posts and post to telegram.

---
+++ Readme.md
@@ -0,0 +1,99 @@
+# Instagram-To-Telegram-Channel
+
+A bot to scrap your instagram feed and post to your Telegram Channel in certain Interval.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://www.herokucdn.com/deploy?template=https://github.com/prabesharyal/Instagram-To-Telegram-Channel/)
+
+# Table of Contents
+ 1. [Introduction](#1)
+    1. [About Bot](#1.1)
+	2. [Working Principle](#1.2)
+ 2. [Knowing All Variables](#2)
+	1. [BOT Token](#2.1)
+    2. [Channel ID](#2.2)
+	3. [Other Notable Variables](#2.3)
+ 4. [Requirements](#3)
+    1. [Python](#3.1)
+		1. [PIPs](#3.1.1)
+ 5. [Deploy](#4)
+ 6. [License](#lic)
+
+
+# Read this throughly before deploying the bot: <a name="1"></a>
+
+## What is this bot about?<a name="1.1"></a>
+This bot is make specifically for one purpose. That is to monitor your Instagram Feed and send defined number of Photos and Videos to you in Telegram at the time interval you specify.
+
+## How does this bot work?<a name="1.2"></a>
+> **This bot works in various steps as :**
+> - It scrapes your Instagram Feed.
+> - Tries to clean captions (beta stage).
+> - Send Caption
+> - And Sends Separate Album
+> - Also, it sends an empty sticker to distinguish between posts.
+		
+# Get Variables <a name="2"></a>
+
+## Bot Token <a name="2.1"></a>
+ - You can get Telegram Bot Token from [BotFather](https://t.me/@BotFather) bot on telegram.
+ 
+## Getting Channel ID <a name="2.2"></a>
+- **To get Channel ID, first go to your channel in Telegram and perform steps mentioned below.**
+- Send a Message to channel and copy link of that Telegram Channel Post.
+> - Then your link looks like this : `https://t.me/c/1617181920/369`
+> - Note that long number of 10 digits and add -100 in beginning and it must look like: `-1001617181920`
+> - This is your Channel ID (including `-` sign.)
+
+ 
+## Other Variables <a name="2.3"></a>
+There are some other variables which you need to note about.
+### Instagram Credentials
+- > Instagram Username and Password
+
+### Variables as configured in bot. <a name="environ"></a>
+- `ig_username` - Enter your Instagram 'Username'
+- `ig_password` - Enter your Instagram 'Password'
+- `BOT_API_TOKEN` - Enter Bot API Token which you get from Botfather in Telegram.
+- `channel` - Enter [Channel ID](#2.2) of your Telegram Channel where you want to send posts.
+- `every_x_minutes` - Enter Time Period at which you want to srape your feed regularly (IN MINUTES)
+- `no_of_posts` - Enter how much posts to be scraped at above time.
+- `Welcome_Message` - Enter some string or welcome message so that you'll know that bot has started working in your channel.
+- `secret_command` - Enter a secret command which you'll send to bot in order to activate. Don't include / here but you must use in Telegram.
+- `every_post_caption` - Enter a Caption for every posts you sent to Telegram in image. Can be your channel username or some emoji.
+
+# Required Softwares and Languages <a name="3"></a>
+
+## Python <a name="3.1"></a>
+> Download Python From here :
+> - [Python Latest Version](https://www.python.org/downloads/)
+
+> *While installing, tick install **path / environment** variables whatever is given*
+
+### Python Snippets <a name="3.1.1"></a>
+- **Install required python modules using commands below:**
+> - `pip install instaloader`
+> - `pip install python-telegram-bot --pre`
+
+- __Install all above modules using :__
+> - `pip install -r requirements.txt`
+
+
+# Deploying the bot <a name="4"></a>
+
+## Deploying Locally
+- Install Python, PIPs using [above methods](#3)
+- Download all files in this repo.
+- Replace variables at the top of `bot.py` file with your ones, removing `os.environ.get('name')` commands.
+
+> Type ***any one*** of the following command on terminal to run bot:
+> - `py bot.py`
+> - `python bot.py`
+> - `python3 bot.py`
+
+## Deploying to HEROKU :
+Press the button below or at the top of this readme and insert necessary [environment variables](#environ) and you're done.
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://www.herokucdn.com/deploy?template=https://github.com/prabesharyal/Instagram-To-Telegram-Channel/)
+
+# License <a name="lic"></a>
+> Distributed Under GPL or MIT License by [@PrabeshAryalNP](https://t.me/prabesharyalnp) on [social](https://twitter.com/prabesharyalnp) or [@PrabeshAryal](https://github.com/prabesharyal) on code sites.
+		
---
+++ bot.py
@@ -0,0 +1,135 @@
+import logging
+from telegram import *
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import os
+import time
+import instaloader
+
+#Environment Variables
+USER = os.environ.get('ig_username')
+PASSWORD = os.environ.get('ig_password')
+API_Hash = os.environ.get('BOT_API_TOKEN')
+CHANNELID = int(os.environ.get('channel'))
+password = os.environ.get('secret_command')
+time_sleep = int(os.environ.get('every_x_minutes'))
+posts_to_scrape = int(os.environ.get('no_of_posts'))
+channel_bot_start_msg = os.environ.get('Welcome_Message')
+CAPTION = os.environ.get('every_post_caption')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+def scrape_feed():
+    L = instaloader.Instaloader()
+    L.login("{}".format(USER), "{}".format(PASSWORD))      # (login)
+    L.download_feed_posts(max_count=posts_to_scrape, fast_update=True, post_filter=None)
+    print("\n"+"Downloading feed : Done!")
+
+async def post_scrape(update: Update, context: ContextTypes):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="***Bot Started Collecting Posts Successfully!*** \n \n ___Go to your respective channel now___",parse_mode= 'Markdown')    
+    await context.bot.send_message(chat_id=CHANNELID, text=channel_bot_start_msg, parse_mode= 'Markdown')
+    await context.bot.send_sticker(chat_id=CHANNELID, sticker='CAACAgEAAxkBAAEFonRjAwlCFg3_h8NDZn2Yr7l2mu86wAACNAEAApHV8Ec2k-NTOFBhtCkE')
+
+    #Loop Where the main logic begins
+    while True:
+        n = 1
+        checkdownload_dir = ".\\：feed"
+        temp_downloaded_files = os.listdir('.\\：feed\\')
+        for unnecessary_files in temp_downloaded_files:
+            unPath=os.path.join(checkdownload_dir,unnecessary_files)
+            os.remove(unPath)
+            print("Unnecessary files were removed!!")
+        
+        scrape_feed()
+        print("Sending from feed. \n")
+        time.sleep(30)
+        download_dir = ".\\：feed"
+        downloaded_files = os.listdir('.\\：feed\\')
+        downloads = []
+        for lists in downloaded_files:
+            files = os.path.join(download_dir,lists)
+            downloads.append(files)
+        #print(downloads)
+
+        media_group = []
+        temp_list = list()
+        postcount = 0
+        for posts in downloads:
+            if posts.endswith(".mp4"):
+                #print(posts)
+                media_group.append(InputMediaVideo(open(posts,'rb'),caption = CAPTION if postcount == 0 else ''))
+                postcount += 1
+                temp_list.append(posts)
+            elif posts.endswith(".jpg"):
+                #print(posts)
+                media_group.append(InputMediaPhoto(open(posts,'rb'), caption = CAPTION if postcount == 0 else ''))
+                postcount += 1
+                temp_list.append(posts)
+            elif posts.endswith(".txt"):
+                #Opening Text file
+                a= open(posts,'r',encoding="utf8")
+                caption = a.read()
+                sdsd = caption.split(" ")
+                wordcount = 0
+                postcount = 0
+                #Removing unncessary stuffs
+                for words in sdsd:
+                    if words[:1]=="#" or words[:1]=="@" or words[:1]=="\n" or words[:1]=="©" or words[:2]=="##" or words[:1]=="." or words[:2]=="  " or words[:1]==".":
+                        break
+                    else:
+                        wordcount = wordcount +1 
+                while len(sdsd)-1 >= wordcount:
+                    sdsd.remove(sdsd[wordcount])
+                    wordcount = wordcount + 1
+                sentence = ' '.join(sdsd)
+                first_file = temp_list[0]
+                if len(temp_list)==1:
+                    if first_file.endswith(".jpg"):
+                        print("Sending Photo as single file")
+                        await context.bot.send_photo(chat_id = CHANNELID, photo=open(first_file, 'rb'), caption="▶ " + sentence+"\n"+CAPTION)
+                        print("Sent {} Post".format(str(n)))
+                        n =+1
+                    os.remove(first_file)
+                    media_group.clear()
+                    temp_list.clear()
+                    time.sleep(30)
+                else:
+                    await context.bot.send_message(chat_id=CHANNELID, text="▶ "+sentence)
+                    #print(media_group)
+                    await context.bot.send_media_group(chat_id = CHANNELID, media = media_group)
+                    media_group.clear()
+                    for files in temp_list:
+                        print("Removed already sent files.")
+                        os.remove(files)
+                    temp_list.clear()
+                    print("Sent {} Post".format(str(n)))
+                    n += 1
+                    time.sleep(20)
+                    await context.bot.send_sticker(chat_id=CHANNELID, sticker='CAACAgEAAxkBAAEFonRjAwlCFg3_h8NDZn2Yr7l2mu86wAACNAEAApHV8Ec2k-NTOFBhtCkE')
+                    time.sleep(20)
+                a.close()
+                print("Removed Text File \n \n")
+                os.remove(posts)
+            else:
+                print("Removed Clutter Files.")
+                os.remove(posts)      
+        print("I need a rest of {} minutes.".format(time_sleep))
+        n =0
+        time.sleep(time_sleep*60)
+
+async def start(update: Update, context: ContextTypes):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="***Bot Started Successfully.*** \n \n ___Enter password as command to run in Channel___",parse_mode= 'Markdown')
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(API_Hash).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    mid_handler = CommandHandler(password, post_scrape)
+    application.add_handler(mid_handler)
+    
+    application.run_polling()
